96d6b9bad4771e4dd74929498fc683999f224842
keshav <keshawcs@gmail.com>
2019-12-11 16:58:45 +0530

    Count Vowel Consonant SpecailCharacter

---
+++ String/src/CntVowConsUsingCapticalLetterString.java
@@ -0,0 +1,36 @@
+
+public class CntVowConsUsingCapticalLetterString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String x="KESHAV'S KATALYST TECHNOLOGY";
+		char []y=x.toCharArray();
+		int size=y.length;
+		int volCnt=0;
+		int consCnt=0;
+		int splCnt=0;
+		int i=0;
+		while(i!=size) {
+			if(y[i]>='A'&& y[i]<='Z') {
+				if(y[i]=='A'||y[i]=='E'||y[i]=='I'||y[i]=='O'||y[i]=='U') {
+					volCnt++;
+				}
+				else {
+					consCnt++;
+				}
+				
+			}
+			else {
+				splCnt++;
+			}
+			i++;
+			
+		}
+		System.out.println(volCnt);
+		System.out.println(consCnt);
+		System.out.println(splCnt);
+
+	}
+
+}
