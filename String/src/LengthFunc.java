0de90eac74cb15675d9903fd990cb847dc664408
keshav <keshawcs@gmail.com>
2019-12-11 11:24:48 +0530

    Using The Length Fuction

---
+++ String/src/LengthFunc.java
@@ -0,0 +1,12 @@
+
+public class LengthFunc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x="Keshav";
+		
+		System.out.println("Length of the string:" +x.length());
+
+	}
+
+}
