7b7c2778f7389d8ecee649765fce8d77fcc62f2d
keshav <keshawcs@gmail.com>
2019-12-12 18:10:39 +0530

    words count

---
+++ src/CountWord.java
@@ -0,0 +1,13 @@
+
+public class CountWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String x="THIS IS KESHAV RAJPUT";
+		String y[]=x.split(" ");
+		System.out.println(y.length);
+
+	}
+
+}
