bb1dca03fcf96e7cc05b3c5aa5ed503c46998f7e
keshav <keshawcs@gmail.com>
2019-12-12 18:11:25 +0530

    Muttable String

---
+++ src/MuttableString.java
@@ -0,0 +1,14 @@
+
+public class MuttableString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer x=new StringBuffer("HELLO ");
+		StringBuffer y=new StringBuffer("KESHAV");
+		x.append(y);
+	System.out.println(x);
+	System.out.println(y);
+
+	}
+
+}
