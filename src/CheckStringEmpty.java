742dd246f58c68369631aa91e3eae290aff694a4
keshav <keshawcs@gmail.com>
2019-12-12 17:40:07 +0530

    Check string is empty or not

---
+++ src/CheckStringEmpty.java
@@ -0,0 +1,19 @@
+
+public class CheckStringEmpty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x="Keshav";
+		int len=x.length();
+		
+		if(len==0) {
+			
+			System.out.println("String is empty");
+		}
+		else {
+			System.out.println("String is not empty");
+		}
+
+	}
+
+}
