3abf6fe101deedaeb39df35664a3ea53ce8781bf
keshav <keshawcs@gmail.com>
2019-12-12 17:39:37 +0530

    Remove White Spaces

---
+++ src/RemoveSpaces.java
@@ -0,0 +1,12 @@
+
+public class RemoveSpaces {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x="THIS IS   KESHAV   RAJPUT";
+		x=x.replaceAll("\\s", "");
+		System.out.println(x);
+
+	}
+
+}
