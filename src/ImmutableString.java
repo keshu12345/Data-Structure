10688952518b57708d77222736d6e1816e49f586
keshav <keshawcs@gmail.com>
2019-12-12 18:11:04 +0530

    Immutable String Check

---
+++ src/ImmutableString.java
@@ -0,0 +1,14 @@
+
+public class ImmutableString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x="KESHAV";
+		String y="RAJPUT";
+		x.concat(y);
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
