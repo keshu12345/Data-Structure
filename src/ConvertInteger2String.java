59c785e536bb8847edf4f149c3ff5d61169cf4ed
keshav <keshawcs@gmail.com>
2019-12-13 11:13:38 +0530

    Convert Integer to String

---
+++ src/ConvertInteger2String.java
@@ -0,0 +1,13 @@
+
+public class ConvertInteger2String {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=65;
+		String y=Integer.toString(x);
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
